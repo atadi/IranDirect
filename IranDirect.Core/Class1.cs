b17550c015f716d67a0f4e9a51e26f0df19a8324
Alireza Tadi <32592502+atadi@users.noreply.github.com>
2026-07-14 07:44:08 +0330

    Create initial IranDirect solution

---
+++ IranDirect.Core/Class1.cs
@@ -0,0 +1,6 @@
+﻿namespace IranDirect.Core;
+
+public class Class1
+{
+
+}
---
+++ IranDirect.Tray/Form1.cs
@@ -0,0 +1,9 @@
+namespace IranDirect.Tray;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
